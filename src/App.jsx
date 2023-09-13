fbb461cf0f4eab640788a553c071e61532970843
N.Botirovna <119905460+N-Botirovna@users.noreply.github.com>
2023-09-13 03:52:29 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-red-600">Hello</h1>
+    </>
+  );
+}
+
+export default App;
